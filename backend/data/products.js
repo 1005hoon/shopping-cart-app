e3fa3764c042e57594942ddca569af4d96ff8161
hoon oh <shoh@help-me.kr>
2021-02-01 18:40:55 +0900

    created data file to import and seeder for presentational purpose

---
+++ backend/data/products.js
@@ -0,0 +1,29 @@
+const products = [
+  {
+    name: "실버 알루미늄 케이스, 그리고 Nike 스포츠 루프",
+    image: "/images/watch_1.png",
+    price: 10000,
+  },
+  {
+    name: "실버 알루미늄 케이스, 그리고 Nike 스포츠 밴드",
+    image: "/images/watch_2.png",
+    price: 10000,
+  },
+  {
+    name: "실버 스테인리스 스틸 케이스, 그리고 모던 버클",
+    image: "/images/watch_3.png",
+    price: 8500,
+  },
+  {
+    name: "골드 알루미늄 케이스, 그리고 브레이드 솔로 루프",
+    image: "/images/watch_4.png",
+    price: 5000,
+  },
+  {
+    name: "스페이스 그레이 알루미늄 케이스, 그리고 스포츠 밴드",
+    image: "/images/watch_5.png",
+    price: 7000,
+  },
+];
+
+export default products;
