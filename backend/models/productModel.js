873fbfbd0ed7e3635f8ab96b1151895f1b5a197f
hoon oh <shoh@help-me.kr>
2021-02-01 18:38:19 +0900

    created model for product

---
+++ backend/models/productModel.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const productSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+});
+
+const Product = mongoose.model("Product", productSchema);
+export default Product;
