3cadead2ca1b6d3074928bf980cf4c04898231c9
Hoon Oh <shoh@help-me.kr>
2021-02-01 22:03:55 +0900

    add query to controller

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
 import morgan from "morgan";
+import connectDB from "./config/db.js";
 
 dotenv.config();
+connectDB();
 const app = express();
 
 app.use(express.json());
--- backend/controller/productController.js
+++ backend/controller/productController.js
@@ -1,11 +1,13 @@
 import asyncHandler from "express-async-handler";
+import Product from "../models/productModel.js";
 
 // @desc      Fetch all products
 // @route     GET   /api/v1/products
 export const fetchAllProducts = asyncHandler(async (req, res, next) => {
+  const products = await Product.find();
   res.status(200).json({
     success: true,
-    data: "get all products",
+    data: products,
   });
 });
 
@@ -13,8 +15,14 @@ export const fetchAllProducts = asyncHandler(async (req, res, next) => {
 // @route     GET   /api/v1/products/:id
 export const fetchProduct = asyncHandler(async (req, res, next) => {
   const id = req.params.id;
-  res.status(200).json({
-    success: true,
-    data: `get product with id ${id}`,
-  });
+  const product = await Product.findById(id);
+  if (product) {
+    return res.status(200).json({
+      success: true,
+      data: product,
+    });
+  } else {
+    res.status(404);
+    throw new Error("해당 물품을 찾을 수 없습니다");
+  }
 });
