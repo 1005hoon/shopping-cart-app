8b2271ad6a1fc34d2e1ba0163d9ddc3bf0104885
hoon oh <shoh@help-me.kr>
2021-02-01 18:42:58 +0900

    added description

--- backend/controller/productController.js
+++ backend/controller/productController.js
@@ -1,5 +1,7 @@
 import asyncHandler from "express-async-handler";
 
+// @desc      Fetch all products
+// @route     GET   /api/v1/products
 export const fetchAllProducts = asyncHandler(async (req, res, next) => {
   res.status(200).json({
     success: true,
@@ -7,10 +9,12 @@ export const fetchAllProducts = asyncHandler(async (req, res, next) => {
   });
 });
 
+// @desc      Fetch all products
+// @route     GET   /api/v1/products/:id
 export const fetchProduct = asyncHandler(async (req, res, next) => {
-  // const id = req.params.id
+  const id = req.params.id;
   res.status(200).json({
     success: true,
-    data: "get product for certain id",
+    data: `get product with id ${id}`,
   });
 });
